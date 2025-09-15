a6c800c55da4e2ff11e715774423a66a6fa11a20
Ken <ken@promocollection.com.au>
2025-09-15 16:08:32 +0800

    init
    feat:change app
    
    1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    似此星辰非昨夜，为谁风露立中宵!
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
